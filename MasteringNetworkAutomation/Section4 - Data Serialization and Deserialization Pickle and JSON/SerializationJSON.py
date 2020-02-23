8c56a6ca574db93fcd722287e8149486d812e149
shoaibmerchant89 <shoaibmerchant89@gmail.com>
2020-02-23 11:22:35 +0530

    JSON serialization and deserialization

--- MasteringNetworkAutomation/Section4 - Data Serialization and Deserialization Pickle and JSON/SerializationJSON.py
+++ MasteringNetworkAutomation/Section4 - Data Serialization and Deserialization Pickle and JSON/SerializationJSON.py
@@ -1,13 +1,34 @@
+# Import python library. Use the official python docs documentation for the json library.
+
 import json
 
-friends1 = {"Dan": [20, "London", 32324356], "Maria": [20, "Manchester", 78864823]}
-friends2 = {"Dan": [20, "London", 32324356], "Maria": [20, "Manchester", 78864823]}
-friends3 = {"Dan": [20, "London", 32324356], "Maria": [20, "Manchester", 78864823]}
+friends1 = {"Shoaib": (30, "Pune", 32324356), "Heena": [31, "Kharadi", 78864823]}
+friends2 = {"Arzoo": [26, "Mumbai", 32324356], "Rizwan": [38, "Borivali", 78864823]}
+friends3 = {"Naaz": [52, "Kalina", 32324356], "Shanu": [58, "Dongri", 78864823]}
 friends = (friends1,friends2,friends3)
-enemies = 'These are my enemies'
+enemies = 'No enemies'
 
+# json.dump - write and encode dict objects into a json file
 with open('friends.json', 'w+') as f:
-    json.dump(friends1, f, indent=4)
+    json.dump(friends, f, indent=2)
 
-json_string = json.dumps(friends1, indent=2)
-print(json_string)
+
+# json.dumps - write and encode dict objects into a json string
+json_obj = json.dumps(friends)
+print(json_obj)
+
+
+# json.load - decode and load a json file into a python dict
+with open('friends.json') as f:
+    fl = json.load(f)
+    print(fl)
+
+
+# json.loads - decode and load a json string into a python dict
+fl = json.loads(json_obj)
+print(type(fl[0]['Shoaib']))
+print(fl[0]['Shoaib'])
+
+# json data structures are not the same as python's. Python's tuple is represented as a array in JSON
+print(type(friends[0]['Shoaib']))
+print(friends[0]['Shoaib'])
